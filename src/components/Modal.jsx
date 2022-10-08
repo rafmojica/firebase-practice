c3ea6e7f552ad9d06ae17c5d5d2d49c95725b318
rafa <rafumojica1@gmail.com>
2022-10-08 20:53:53 +0800

    added animation

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -34,33 +34,39 @@ const Modal = ({ auth, registerUser, loginUser }) => {
   }
 
   return (
-    <div className="modal">
-      <h2 className="modal__title">Login / Register</h2>
-      <div className="email">
-        <h4 className="modal__input--title">Email Address</h4>
-        <input
-          type="email"
-          className="input__email modal__input"
-          placeholder="example@email.com"
-          onChange={(event) => {
-            setEmail(event.target.value)
-          }}
-        />
-      </div>
-      <div className="password">
-        <h4 className="modal__input--title">Password</h4>
-        <input
-          type="password"
-          className="input__password modal__input"
-          placeholder="********"
-          onChange={(event) => {
-            setPassword(event.target.value)
-          }}
-        />
-      </div>
-      <div className="button__wrapper">
-        <button onClick={() => login()} className="input__submit btn">Login</button>
-        <button onClick={() => register()} className="input__submit btn">Register</button>
+    <div className="modal__wrapper">
+      <div className="modal">
+        <h2 className="modal__title">Login / Register</h2>
+        <div className="email">
+          <h4 className="modal__input--title">Email Address</h4>
+          <input
+            type="email"
+            className="input__email modal__input"
+            placeholder="example@email.com"
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}
+          />
+        </div>
+        <div className="password">
+          <h4 className="modal__input--title">Password</h4>
+          <input
+            type="password"
+            className="input__password modal__input"
+            placeholder="********"
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}
+          />
+        </div>
+        <div className="button__wrapper">
+          <button onClick={() => login()} className="input__submit btn">
+            Login
+          </button>
+          <button onClick={() => register()} className="input__submit btn">
+            Register
+          </button>
+        </div>
       </div>
     </div>
   );
